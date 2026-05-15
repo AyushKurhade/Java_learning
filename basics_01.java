c439e0bb1e69f7bca64e7e916419a63c1d11c112
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-05-15 19:00:33 +0530

    Basics Bout concatination

---
+++ basics_01.java
@@ -0,0 +1,30 @@
+public class basics_01 {
+    
+ // + works as concate operator 
+  // string + string = string
+   // string + int = string
+    // int + int = int
+    // int + string = string
+
+
+    public static void main (String[] args){
+        int a = 10;
+        int b  = 20;
+        System.out.println("sum="+a+b); // 1020
+        // sum=10        + 20 = sum=1020
+        // string + int = string
+            System.out.println("sum="+(a+b)); // 30
+            // sum= (10+20) = sum=30
+
+
+        String s1 = "12";
+        String s2 = "34";
+        System.out.println(s1+s2); // 1234
+
+
+            // string + string = string 
+
+            
+
+    }
+}
