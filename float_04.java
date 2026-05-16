182c0c56a4d2065c7887d4df8e83185e60756583
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-05-16 17:28:41 +0530

    float precision formatting example

---
+++ float_04.java
@@ -0,0 +1,15 @@
+public class float_04 {
+    
+
+    // charater conversion double to precised float
+    public static void main (String[] args){
+        float f = 5.377777777777f;
+
+        // goal is to convert above value into 5.38
+
+        System.out.printf("%.2f  \n  ", f);
+        System.out.printf("%.2f", f);
+
+        // 2f for presize 2 decimal places and f for float value
+    }
+}
