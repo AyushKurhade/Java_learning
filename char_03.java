48ef2bb8b61503de46e43cb6a7c3200ec8c12d78
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-05-16 17:27:43 +0530

    ASCII values and char casting

---
+++ char_03.java
@@ -0,0 +1,23 @@
+public class char_03 {
+    public static void main(String[] args) {
+        
+
+    // learning about ASCII 
+
+        // A- 65 
+        // a- 97
+        // 0- 48
+
+        char ch = '5';            // 53
+        int result = ch - '0';     // 48 
+        System.out.println(result); // 5
+
+
+        int a = 65;
+         char ch1 = (char) a;
+          System.out.println(ch1);
+        // char c = a; // incompatible type 
+      
+
+    }
+}
