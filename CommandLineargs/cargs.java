1a342bac21450b5dcc82fd3ec6f19f747325f45a
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-06-20 22:09:19 +0530

    added command line args example and docs

---
+++ CommandLineargs/cargs.java
@@ -0,0 +1,20 @@
+// package CommandLineargs;
+
+//  public class cargs {
+
+//     public static void main (String[] args) {
+
+//         for(int i = 0; i <args.length; i++){
+//             System.out.println(args[i]);
+//         }
+
+
+//     }
+// }
+
+// run on cmd
+// cd copy path
+// javac cargs.java
+// java cargs A B C 
+// returns no error
+// returns A B C
---
+++ CommandLineargs/cargs_02.java
@@ -0,0 +1,23 @@
+// package CommandLineargs;
+
+// public class cargs_02 {
+    
+//     public static void main (String[] args) {
+
+//         for(int i = 0; i <= args.length; i++){
+//             System.out.println(args[i]);
+//         }
+
+    
+// }
+
+// run on cmd
+// cd copy path
+// javac cargs_02.java
+// java cargs_02 A B C
+// returns error
+// ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+// arr[0] = A
+// arr[1] = B
+// arr[2] = C
+// arg.length = none 
