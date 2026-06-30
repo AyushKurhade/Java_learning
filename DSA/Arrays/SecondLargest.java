c4718a94841a612fd8229dd7ad5637ecf4194d08
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-06-30 22:59:07 +0530

    feat: add second largest element program in Java

---
+++ DSA/Arrays/SecondLargest.java
@@ -0,0 +1,28 @@
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 45, 67, 23, 67};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            }
+
+            else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No second largest element");
+        } else {
+            System.out.println("Second Largest = " + secondLargest);
+        }
+    }
+}
