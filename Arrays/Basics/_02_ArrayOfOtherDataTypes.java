8fb0804ea8f9244042320d2cf28a4d96d8f57865
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-07-14 23:56:50 +0530

    add : array of String Datatype

---
+++ Arrays/Basics/_02_ArrayOfOtherDataTypes.java
@@ -0,0 +1,14 @@
+package Arrays.Basics;
+
+public class _02_ArrayOfOtherDataTypes {
+    public static void main(String[] args) {
+//        char[] arr = new char[8];
+//        for(int i=0;i<arr.length;i++){
+//            System.out.print(arr[i]+" ");
+//        }
+        String[] arr = {"Ayush","Arnav","Anjali","Vaishnavi","Yash"};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
