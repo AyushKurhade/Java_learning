98a4bab4bf96875e424689b07172d8dac286e67c
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-07-14 23:57:24 +0530

    add : printing the -ve elements in an array

---
+++ Arrays/Basics/_03_printingNegativeElements.java
@@ -0,0 +1,23 @@
+package Arrays.Basics;
+
+import java.util.Scanner;
+
+public class _03_printingNegativeElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of an array : ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0 ; i<n; i++){
+            if(arr[i]<0){
+                System.out.println(arr[i]);
+
+            }
+    }
+}
+
+}
